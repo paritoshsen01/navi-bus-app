5da0f9046013c921f1785f677feecbdaba3eecd2
paritoshsen01 <paritoshsen2006@gmail.com>
2026-01-26 02:41:31 +0530

    Fix Vercel entrypoint to import express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = require("./api/index");
+const express = require("express");
+const app = express();
+
+// import actual backend
+const backendApp = require("./api/index");
+
+// forward all requests to backend app
+app.use(backendApp);
+
+module.exports = app;
